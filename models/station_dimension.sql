406af59bbdebf05b2325ed0f8e445a44a7dc540a
Jo SeongMin <hronting@gmail.com>
2026-04-22 09:39:22 +0000

    correction of the data to match the year 2018

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -2,11 +2,11 @@ WITH BIKE as (
 
 SELECT
 distinct
-start_statio_id AS station_id,
+start_station_id AS station_id,
 start_station_name AS station_name,
 start_lat,
 start_lng
-FROM {{ source('demo', 'bike') }}
+FROM {{ ref('stg_bike') }}
 
 )
 
